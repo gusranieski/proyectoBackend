3ee8441a602c0038f94c12fb734cc6aec592ddeb
gustavo.rañieski <gustavoranieski@hotmail.com>
2023-08-05 12:14:15 -0300

    Entrega Proyecto Final

--- src/dao/db-managers/user.manager.js
+++ src/dao/db-managers/user.manager.js
@@ -1,43 +1,43 @@
-// import userModel from "../models/user.model.js";
+import userModel from "../models/user.model.js";
 
-// export class UserManagerMongo {
-//   constructor() {}
+export class UserManagerMongo {
+  constructor() {}
 
-//   async getAllUsers() {
-//     try {
-//       const users = await userModel.find();
-//       return users;
-//     } catch (error) {
-//       throw error;
-//     }
-//   }
+  async getAllUsers() {
+    try {
+      const users = await userModel.find();
+      return users;
+    } catch (error) {
+      throw error;
+    }
+  }
 
-//   async createUser(userData) {
-//     try {
-//       const newUser = await userModel.create(userData);
-//       return newUser;
-//     } catch (error) {
-//       throw error;
-//     }
-//   }
+  async createUser(userData) {
+    try {
+      const newUser = await userModel.create(userData);
+      return newUser;
+    } catch (error) {
+      throw error;
+    }
+  }
 
-//   async updateUser(userId, userData) {
-//     try {
-//       const updatedUser = await userModel.findByIdAndUpdate(userId, userData, {
-//         new: true,
-//       });
-//       return updatedUser;
-//     } catch (error) {
-//       throw error;
-//     }
-//   }
+  async updateUser(userId, userData) {
+    try {
+      const updatedUser = await userModel.findByIdAndUpdate(userId, userData, {
+        new: true,
+      });
+      return updatedUser;
+    } catch (error) {
+      throw error;
+    }
+  }
 
-//   async deleteUser(userId) {
-//     try {
-//       const deletedUser = await userModel.findByIdAndDelete(userId);
-//       return deletedUser;
-//     } catch (error) {
-//       throw error;
-//     }
-//   }
-// }
+  async deleteUser(userId) {
+    try {
+      const deletedUser = await userModel.findByIdAndDelete(userId);
+      return deletedUser;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
